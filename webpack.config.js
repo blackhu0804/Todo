adbae6e5908d1fce2855a9eb829272fd92e49d9b
huth <812510003@qq.com>
2017-09-10 19:48:48 +0800

    使用 webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,14 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist'
+	},
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/'
 	},
 	module: {
 		rules: [{
