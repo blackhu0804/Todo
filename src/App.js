f7518d5b1a14f525b883acdd08b383001329b078
hu970804 <812510003@qq.com>
2017-09-19 22:34:44 +0800

    add enter event & a bug

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      newTodo: '测试文本',
+      newTodo: '',
       todoList: [
-        {id:1, title: '第一个待办'},
-        {id:2, title: '第二个待办'}
+
       ]
     }
   }
@@ -21,17 +20,20 @@ class App extends Component {
 
     let todos = this.state.todoList.map( (item, index) => {
       return (
-        <li>
+        <li key={index}>
           <TodoItem todo={item} />
         </li>      
       )
     })
+    console.log(todos);
 
     return (
       <div className="App">
         <h1>我的待办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} />  
+          <TodoInput content={this.state.newTodo}
+          onChange={this.changeTitle.bind(this)}
+          onSubmit={this.addTodo.bind(this)} />  
         </div>
         <ol>
           {todos}
@@ -39,6 +41,31 @@ class App extends Component {
       </div>
     );
   }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    })
+  }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    })
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    })
+  }
 }
 
 export default App;
+
+let id = 0;
+
+function idMaker(){
+  id += 1;
+  return id;
+}
