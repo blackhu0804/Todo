0f04b72f8f9ee653a1f0729b653a58e95b730f5d
hu970804 <812510003@qq.com>
2017-09-22 13:43:21 +0800

    add localStorage 功能

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './reset.css'
 import 'normalize.css'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
+import * as localStore from './localStorage'
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       newTodo: '',
-      todoList: [     
-      ]
+      todoList: localStore.load('todoList') || []
     }
   }
 
@@ -45,13 +45,15 @@ class App extends Component {
   
   toggle(e, todo){
     todo.status = todo.status === 'completed' ? '' : 'completed'
-    this.setState(this.state)     
+    this.setState(this.state)    
+    localStore.save('todoList', this.state.todoList) 
   }
   changeTitle(event) {
     this.setState({
       newTodo: event.target.value,
       todoList: this.state.todoList
     })
+    localStore.save('todoList', this.state.todoList) 
   }
   addTodo(event) {
     this.state.todoList.push({
@@ -64,10 +66,12 @@ class App extends Component {
       newTodo: '',
       todoList: this.state.todoList
     })
+    localStore.save('todoList', this.state.todoList) 
   } 
   delete(e, todo) {
     todo.deleted = true
     this.setState(this.state)
+    localStore.save('todoList', this.state.todoList) 
   }
 }
 
