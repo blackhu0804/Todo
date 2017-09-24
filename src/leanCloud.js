fda01a6cea334a67c8c0d2d79a8288c0963ecb68
hu970804 <812510003@qq.com>
2017-09-24 22:16:22 +0800

    remove tese code and add leanCloud.js

---
+++ src/leanCloud.js
@@ -0,0 +1,12 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'dOE4mM5VcdfVGpzRRS7AWqsM-gzGzoHsz';
+var APP_KEY = 'rpeefKpR5Fq7T3kTsBeyeRad';
+
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+
+
+export default AV;
