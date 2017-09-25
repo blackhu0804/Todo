9cad1087ad7972a7d5e0adf3ff81cbaeb798a462
hu970804 <812510003@qq.com>
2017-09-25 20:08:38 +0800

    消除警告，记住username

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      user: {},
       newTodo: '',
       todoList: []
     }
@@ -30,7 +31,7 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>我的待办</h1>
+        <h1>{this.state.user.username||'我'}的待办</h1>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo} 
           onChange={this.changeTitle.bind(this)}
@@ -39,9 +40,15 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-        <UserDialog />
+        <UserDialog onSignUp={this.onSignUp.bind(this)} />
       </div>
     )
+  }
+  onSignUp(user){
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = user
+    this.setState(stateCopy)
+    console.log(this.state.user.username)
   }
   componentDidUpdate(){
     //在组件更新后调用
