5b5624575ed0d57c1388752ace704f4915c22c05
hu970804 <812510003@qq.com>
2017-10-05 19:48:01 +0800

    在 constructor 中发起请求获取所有 Todo

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -21,6 +21,18 @@ export function signIn(username, password, successFn, errorFn){
 }
 
 export const TodoModel = {
+  getByUser(user, successFn, errorFn){
+    let query = new AV.Query('Todo')
+    query.find().then((response) => {
+      let array = response.map((t) => {
+        return {id: t.id, ...t.attributes}
+      })
+      successFn.call(null, array)
+    }, (error) => {
+      errorFn && errorFn.call(null, error)
+    })
+  },
+
   create({status, title, deleted}, successFn, errorFn){
     let Todo = AV.Object.extend('Todo')
     let todo = new Todo()
